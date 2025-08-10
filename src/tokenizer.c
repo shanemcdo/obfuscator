0cb1ac6f35c641e0d5e9cadc238f3ba1083416fd
Shane McDonough <shanemcdman@gmail.com>
2025-08-09 21:31:09 -0700

    Revert "fix issue with function like macros not working"
    jk it's still broken
    apparently the full macro (arguments and all) need to be included in the
    substitution
    
    This reverts commit bf8e4d3022747d692dba53acd40b04682902ab2b.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -7,6 +7,7 @@
 
 // TODO: KNOWN BUG: Macros will not work correcty when there are #ifdefs in the code
 // TODO: KNOWN BUG: Name collisions can occur with already existing names in #define macros
+// TODO: KNOWN BUG: Function-like macros will not work because there cannot be a space between name and PAREN
 
 typedef enum {
 	NEWLINE,
@@ -178,11 +179,7 @@ int get_tokens(int *out_len, char ***out_array, char **out_macros) {
 			case EOF: // TODO: might be unnecessiary
 				break;
 			default:
-				if(ch == '(') {
-					token[token_index++] = ch;
-				} else {
-					CHECK_UNGETC(ch);
-				}
+				CHECK_UNGETC(ch);
 				(*out_array)[array_index] = strdup(token);
 				if((*out_array)[array_index++] == NULL) return ERR_MEM;
 				bzero(token, sizeof(token) / sizeof(*token));
